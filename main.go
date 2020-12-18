32796e9bf2eb64db9704b3abefee10e3cfae2c08
foxsir <838394225@qq.com>
2020-12-18 19:20:40 +0800

    update

--- main.go
+++ main.go
@@ -11,26 +11,50 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
-func main() {
+type Docker struct {
+	cli *client.Client
+	ctx context.Context
+}
 
+func main() {
 	if len(os.Args) != 3 {
 		fmt.Println("bad num of arguments:\n\t1. = dir with image content\n\t2. = image name")
 		os.Exit(0)
 	}
 
-	msg, err := buildImage(os.Args[1], os.Args[2])
+	client := Docker{}.client()
+	msg, err := client.buildImage(os.Args[1], os.Args[2])
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	//cli := Docker{}.client().cli
+
 	fmt.Println(msg)
 
+	fmt.Println("\n\n")
+
+	client.rebuildImage(os.Args[2])
+}
+
+func (docker Docker) client() Docker {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	docker.cli = cli
+	docker.ctx = context.Background()
+	defer docker.cli.Close()
+
+	return docker
 }
 
 func createTar(srcDir, tarFIle string) error {
@@ -51,7 +75,33 @@ func tempFileName(prefix, suffix string) (string, error) {
 	return filepath.Join(os.TempDir(), prefix+hex.EncodeToString(randBytes)+suffix), nil
 }
 
-func buildImage(dir, name string) ([]string, error) {
+func (docker Docker) rebuildImage(name string) {
+	s, _, _ := docker.cli.ImageInspectWithRaw(docker.ctx, name)
+	fmt.Println(s.Config.Cmd)
+	fmt.Println(s.Config.Entrypoint)
+	fmt.Println(s.Config.Env)
+
+	env := []byte(strings.Join(s.Config.Env, "\", \""))
+	envstr := []string{"ENV [\"", string(env), "\"]"}
+
+	f, err := os.OpenFile("dat1", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	//if _, err = f.WriteString("text"); err != nil {
+	//	panic(err)
+	//}
+
+	f.WriteString(strings.Join(envstr, ""))
+	f.WriteString("\n\n")
+	f.WriteString(strings.Join(envstr, ""))
+
+}
+
+func (docker Docker) buildImage(dir, name string) ([]string, error) {
 
 	tarFile, err := tempFileName("docker-", ".image")
 	if err != nil {
@@ -70,13 +120,7 @@ func buildImage(dir, name string) ([]string, error) {
 	}
 	defer dockerFileTarReader.Close()
 
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	defer cli.Close()
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(300)*time.Second)
+	ctx, cancel := context.WithTimeout(docker.ctx, time.Duration(300)*time.Second)
 	defer cancel()
 
 	buildArgs := make(map[string]*string)
@@ -91,7 +135,7 @@ func buildImage(dir, name string) ([]string, error) {
 		return nil, err
 	}
 
-	resp, err := cli.ImageBuild(
+	resp, err := docker.cli.ImageBuild(
 		ctx,
 		dockerFileTarReader,
 		types.ImageBuildOptions{
